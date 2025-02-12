9de69613d7b7e036288ee9340a36f6f90854e238
Diego Lauar <diego_pktd@hotmail.com>
2025-02-12 10:13:32 -0300

    Fazendo validacao do formulario

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,44 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod"
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
+
+//Formato sem utilizar o zod
+
+// interface newCycleFormData {
+//     task: string
+//     minutesAmount: number
+// }
+
+type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm()
+    const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0
+        }
+    })
     
-    function handleCreateNewCycle(data: any) {
+    function handleCreateNewCycle(data: newCycleFormData) {
         console.log(data)
+        reset()
     }
     
     const task = watch('task')
     const isSubmitDisabled = !task
+
+    
 
     return (
         <HomeContainer>
